5147f56e74dbe0529ce83b8109af929b0aa89d59
deepanshulamba <lamba9374@gmail.com>
2018-03-20 14:53:00 +0530

    spychat

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello world")
+print 'what\'s up?'
+print 'Let\'s get started...'
+spy_name=input("What is your name?")
+print 'Welcome ' + spy_name + '. Glad to have you back with us.'
+spy_salutation = input("What should we call you (Mr. or Ms.)?")
+spy_salutation + " " + spy_name
+
+spy_name = spy_salutation + " " + spy_name
+print(spy_name)
+
